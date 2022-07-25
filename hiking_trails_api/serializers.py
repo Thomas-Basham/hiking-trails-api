1d53e05a559ce5f4dee83168ff02ecb31f68ef74
Thomas Basham <Bashamtg@gmail.com>
2022-07-25 16:52:58 -0700

    updated login/registration

--- hiking_trails_api/serializers.py
+++ hiking_trails_api/serializers.py
@@ -4,5 +4,5 @@ from .models import HikingTrails
 
 class HikingTrailsSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ("id", "owner", "trail_name", "description", "lat", "lon", "google_maps_directions", "all_trails_link")
+        fields = ("id", "owner", "trail_name", "description", "lat", "lon", "google_maps_directions", "wta_link")
         model = HikingTrails
