59536944af1df427a00890ffe80733a0df137154
Thomas Basham <Bashamtg@gmail.com>
2022-07-25 16:16:10 -0700

    updated login

--- hiking_trails_api_project/forms.py
+++ hiking_trails_api_project/forms.py
@@ -10,11 +10,52 @@ class NewUserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("email", "password1", "password2")
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
+        user.email = self.cleaned_data['email'].lower()
+        user.username = self.cleaned_data['email'].lower()
+
         if commit:
             user.save()
         return user
+    
+    
+# uses django's default Authentication procedure, sending email as username
+# rest of function lives in backends.by
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = UsernameField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'someone@email.com', 'for': 'Email'}
+    ),
+        label=_("Email")
+
+    )
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': '',
+        }
+    ))
+
+
+# So that you can log into admin with email instead of username
+class CustomAdminAuthenticationForm(AdminAuthenticationForm):
+    """
+    A custom authentication form used in the admin app.
+    """
+    error_messages = {
+        **AuthenticationForm.error_messages,
+        'invalid_login': _(
+            "Please enter the correct phone number or email and password for a staff "
+            "account. Note that both fields may be case-sensitive."
+        ),
+    }
+
+    username = UsernameField(
+        label='Email',
+        widget=forms.TextInput(attrs={'autofocus': True})
+    )
--- hiking_trails_map/views.py
+++ hiking_trails_map/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django_pandas.io import read_frame
-from hiking_trails_api_project.forms import NewUserForm
+from hiking_trails_api_project.forms import NewUserForm, UserLoginForm
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
@@ -70,20 +70,21 @@ def register_request(request):
 
 def login_request(request):
     if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
+        form = UserLoginForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
+            username = username.lower()
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
                 messages.info(request, f"You are now logged in as {username}.")
-                return redirect("maps")
+                return redirect("homepage")
             else:
                 messages.error(request, "Invalid username or password.")
         else:
             messages.error(request, "Invalid username or password.")
-    form = AuthenticationForm()
+    form = UserLoginForm()
     return render(request=request, template_name="login.html", context={"login_form": form})
 
 
