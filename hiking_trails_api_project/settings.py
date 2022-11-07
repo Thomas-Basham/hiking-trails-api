adda41212ff328633597ee9175bd92320f56e032
Thomas Basham <Bashamtg@gmail.com>
2022-11-07 10:56:10 -0800

    changed authentication settings

--- hiking_trails_api_project/settings.py
+++ hiking_trails_api_project/settings.py
@@ -163,7 +163,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",  # Change from .AllowAny to IsAuthenticated
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ]
 }
 
