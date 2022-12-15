d7a35b2645ca48d89702af676025650cff56e5a0
Thomas Basham <Bashamtg@gmail.com>
2022-12-15 13:36:58 -0800

    updated settings

--- hiking_trails_api_project/settings.py
+++ hiking_trails_api_project/settings.py
@@ -51,7 +51,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://trails-api-thomas-basham.herokuapp.com']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://trails-api-thomas-basham.herokuapp.com']
 
 # Application definition
 
