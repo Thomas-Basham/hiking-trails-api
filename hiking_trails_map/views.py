c34a9d979af4631ef2385be96b0393594e1d96c8
Thomas Basham <Bashamtg@gmail.com>
2023-01-25 09:19:31 -0800

    refactored

--- hiking_trails_map/views.py
+++ hiking_trails_map/views.py
@@ -10,46 +10,50 @@ from django.forms import ModelForm
 from django.views.generic import (
   UpdateView
 )
-import pandas as pd
 import folium
 
 
 def maps(request):
   current_user = request.user
-  df = pd.DataFrame(list(HikingTrails.objects.all().values()))
-  print(df)
-  map = folium.Map(zoom_start=7, location=df[["lat", "lon"]].astype(
-    'float').mean().to_list())  # Starts zoom at average of lat/lon from pandas
-  marker_cluster = MarkerCluster().add_to(map)  # Start a cluster to add to the map
-  for i, r in df.iterrows():
-    if current_user.id == r["owner_id"]:
+  hiking_trails = HikingTrails.objects.all()
+  total_lat = 0
+  total_lon = 0
+  trail_count = 0
+  for trail in hiking_trails:
+    total_lat += float(trail.lat)
+    total_lon += float(trail.lon)
+    trail_count += 1
+  map_center = [total_lat / trail_count, total_lon / trail_count]
+  map = folium.Map(zoom_start=7, location=map_center)
+  marker_cluster = MarkerCluster().add_to(map)
+  for trail in hiking_trails:
+    if current_user.id == trail.owner_id:
       edit_button = f'''
-        <a style="float: right" href="{r["id"]}/trail-update/" title="Edit Trail" target="_parent">
-          <svg style="width: 20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-1 h-1">
-            <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
-            <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
-          </svg
-        </a>
-                       </a>
-        '''
+              <a style="float: right" href="{trail.id}/trail-update/" title="Edit Trail" target="_parent">
+                <svg style="width: 20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-1 h-1">
+                  <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
+                  <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
+                </svg
+              </a>
+                             </a>
+              '''
     else:
       edit_button = ''
-
     html = f'''
-        {edit_button}
-        <h2 style="cursor: default" >{r["trail_name"].capitalize()}<h2/>
-        <small  style="cursor: default">Added by <strong>{User.objects.get(id=r["owner_id"])}</strong> </small>
-        <br>
-        <a style="color:blue" href="{r["google_maps_directions"]}" target="_blank" rel="nofollow" >Directions via Googlemaps <a/>
-        <br>
-        <a style="color:green" href="{r["wta_link"]}" target="_blank">Link to WTA Page<a/>
-        <p >{r["description"]}<p/>
-        '''
+          {edit_button}
+          <h2 style="cursor: default" >{trail.trail_name.capitalize()}<h2/>
+          <small  style="cursor: default">Added by <strong>{User.objects.get(id=trail.owner_id)}</strong> </small>
+          <br>
+          <a style="color:blue" href="{trail.google_maps_directions}" target="_blank" rel="nofollow" >Directions via Googlemaps <a/>
+          <br>
+          <a style="color:green" href="{trail.wta_link}" target="_blank">Link to WTA Page<a/>
+          <p >{trail.description}<p/>
+          '''
     popup = folium.Popup(html, max_width=500)
 
-    location = (r["lat"], r["lon"])
+    location = (trail.lat, trail.lon)
 
-    folium.Marker(location=location, tooltip=r["trail_name"].capitalize(), popup=popup,
+    folium.Marker(location=location, tooltip=trail.trail_name.capitalize(), popup=popup,
                   ).add_to(marker_cluster)
 
   Fullscreen(
